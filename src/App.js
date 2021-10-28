a0d2eb3bd2f26095a5fcc055b44a8e2731999062
HikaruFN <francesconisi95@gmail.com>
2021-10-28 16:36:33 +0200

    armonia-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+//questo componente sarà il wrapper dell'intera pagina
 import './App.css';
-
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <Sidebar></Sidebar>
+        <MainContent></MainContent>
+      </div>
     </div>
   );
 }
-
 export default App;
